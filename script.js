0032629d268d1a2f6034f51b3f7b3e2a37e42ecf
Gabin Demé <129855477+Onibagg@users.noreply.github.com>
2023-06-27 11:12:52 +0200

    vertical nav

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-const themeToggle = document.getElementById("theme-toggle");
-const sections = document.querySelectorAll("section");
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('nav');
+const sections = document.querySelectorAll('section');
 
-themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    themeToggle.setAttribute("aria-label", document.body.classList.contains("dark-mode") ? "Toggle Light Mode" : "Toggle Dark Mode");
+navToggle.addEventListener('click', () => {
+    navToggle.classList.toggle('open');
+    nav.classList.toggle('open');
 });
 
 // Animation à l'entrée de chaque section
 sections.forEach((section) => {
     section.style.opacity = 0;
-    section.style.animation = "fadeIn 0.5s forwards";
+    section.style.animation = 'fadeIn 0.5s forwards';
 });
 
 // Animation au changement de section
-window.addEventListener("hashchange", () => {
+window.addEventListener('hashchange', () => {
     sections.forEach((section) => {
         section.style.opacity = 0;
-        section.style.animation = "fadeOut 0.3s forwards";
+        section.style.animation = 'fadeOut 0.3s forwards';
     });
 
     const currentSection = document.querySelector(window.location.hash);
-    currentSection.style.animation = "fadeIn 0.5s forwards";
+    currentSection.style.animation = 'fadeIn 0.5s forwards';
 });
