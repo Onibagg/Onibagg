eba4edeb5e7a350dcee557fd4f8cf29ccd19bb82
Gabin Demé <129855477+Onibagg@users.noreply.github.com>
2023-07-04 10:09:29 +0200

    cleanup

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-window.addEventListener('scroll', function() {
-    var scrollMessage = document.getElementById('scrollMessage');
-    scrollMessage.style.opacity = '0';
+window.addEventListener('scroll', function () {
+  var scrollMessage = document.getElementById('scrollMessage');
+  scrollMessage.style.opacity = '0';
+});
+
+
+$(window).scroll(function () {
+  var winScroll = $(this).scrollTop();
+  // console.log(winScroll);
+  $('.bg').css({
+    'transform': 'translate(0,' + winScroll / 14 + '%)'
   });
-  
+  $('.bg2').css({
+    'transform': 'translate(0,' + winScroll / 20 + '%)'
+  });
+  $('.bg3').css({
+    'transform': 'translate(0,' + winScroll / 35 + '%)'
+  });
+  $('.bg4').css({
+    'transform': 'translate(0,' + winScroll / 70 + '%)'
+  });
+  $('.bg5').css({
+    'transform': 'translate(0,' + winScroll / 700 + '%)'
+  });
+});
