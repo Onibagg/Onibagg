330a87cf2aea711e2e13d12e4910f06d91810323
Gabin Demé <129855477+Onibagg@users.noreply.github.com>
2024-08-06 17:06:42 +0200

    new case
    deletion of banner
    adding of projects

--- Terminal/main.js
+++ Terminal/main.js
@@ -71,7 +71,7 @@ document.addEventListener('click', function(event) {
 document.getElementById('inputArea').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         event.preventDefault(); // Empêche le retour à la ligne dans la textarea
-        var inputText = this.value.trim(); // Utilisez trim ici pour nettoyer l'entrée dès le départ
+        var inputText = this.value.trim(); // Trim ici pour nettoyer l'entrée dès le départ
         
         switch (inputText) {
             case 'clear':
@@ -81,7 +81,7 @@ document.getElementById('inputArea').addEventListener('keydown', function(event)
                 document.getElementById('output').innerHTML += 'visitor@gabindeme.com:~$ ' + '<br>'; // Ajoute simplement une nouvelle ligne
                 break;
             case 'help':
-                document.getElementById('output').innerHTML += 'visitor@gabindeme.com:~$ ' + inputText + '<p id="help_out">Available commands: <gab id="glow_cmd">clear</gab>, <gab id="glow_cmd">help</gab>, <gab id="glow_cmd">cv</gab></p>';
+                document.getElementById('output').innerHTML += 'visitor@gabindeme.com:~$ ' + inputText + '<p id="help_out">Available commands: <gab id="glow_cmd">clear</gab>, <gab id="glow_cmd">help</gab>, <gab id="glow_cmd">cv</gab>, <gab id="glow_cmd">projects</gab></p>';
                 break;
             case 'cv':
                 document.getElementById('output').innerHTML += 'visitor@gabindeme.com:~$ ' + inputText + '<br><p id="cv_out">Downloading my CV...<p>';
@@ -93,12 +93,63 @@ document.getElementById('inputArea').addEventListener('keydown', function(event)
                 link.click();
                 document.body.removeChild(link);
                 break;
-            case 'banner':
-                document.getElementById('output').innerHTML += 'visitor@gabindeme.com:~$ ' + inputText + '<p id="banner_out>';
-                var outputDiv = document.getElementById('output');
-                outputDiv.innerHTML += `<pre>${mergedBanner}</pre>`;
-                outputDiv.innerHTML += 'Welcome to Gabin Demé\'s Terminal!<br>';
-                outputDiv.innerHTML += 'Type <gab id="glow_cmd">help</gab> to view a list of available commands.</p><br><br>';
+            case 'projecs':
+                var projects = [
+                    {
+                        title: "gabindeme.com",
+                        description: "This very own website😄.",
+                        demoLink: "https://gabindeme.com/",
+                        githubLink: "https://github.com/Onibagg/Onibagg"
+                    },
+                    {
+                        title: "SAE23-Groupe7",
+                        description: "This is my first PHP project. It's a showcase website with a PHP intranet that hosts a file explorer.",
+                        demoLink: "#",
+                        githubLink: "https://github.com/Onibagg/SAE23-Groupe7"
+                    },
+                    {
+                        title: "NotationJeuVideo",
+                        description: "This is my first MySQL project — a PHP-based game scoring website seamlessly integrated with MySQL.",
+                        demoLink: "http://example.com/demo2",
+                        githubLink: "https://github.com/Onibagg/NotationJeuVideo"
+                    }
+                ];
+                
+
+                document.getElementById('output').innerHTML += 'visitor@gabindeme.com:~$ ' + inputText + '<br>';
+                projects.forEach(project => {
+                    document.getElementById('output').innerHTML += `
+                        <p id="project_out">
+                            <strong>${project.title}</strong><br>
+                            ${project.description}<br>
+                            <span class="link-container">
+                                <a href="${project.demoLink}" target="_blank" class="centered-link glow-cmd">
+                                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
+                                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                        <g id="SVGRepo_iconCarrier">
+                                            <g id="Interface / External_Link">
+                                                <path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                            </g>
+                                        </g>
+                                    </svg> Demo
+                                </a> |
+                                <a href="${project.githubLink}" target="_blank" class="centered-link glow-cmd">
+                                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
+                                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                        <g id="SVGRepo_iconCarrier">
+                                            <g id="Interface / External_Link">
+                                                <path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                            </g>
+                                        </g>
+                                    </svg> Code
+                                </a>
+                            </span>
+                        </p>
+
+                    `;
+                });
                 break;
             default:
                 document.getElementById('output').innerHTML += 'visitor@gabindeme.com:~$ ' + inputText + `<br><p id="false_1">command not found: ${inputText}</p><p id="false_2">Type <gab id="glow_cmd">help</gab> to view a list of available commands.</p><br>`;
